76ff2343bea2e5bff081f90ebfad8ae724d48357
vmolchanova <139213113+mvarukha@users.noreply.github.com>
2025-11-09 19:05:15 +0700

    Update task10.py
    wcomments

--- task10.py
+++ task10.py
@@ -1,27 +1,34 @@
 def main() -> None:
     """
-    Считывает две строки (целые числа
-    через пробел) и диапазон, выполняет
-    преобразования и выводит результаты.
+    Считывает два списка и диапазон, выполняет преобразования.
+    Диапазон: start_index и end_index (включительно) от 1 до N.
     """
-    list1 = list(map(int, input().split()))
-    list2 = list(map(int, input().split()))
-
-    start_index = int(input()) - 1
-    end_index = int(input()) - 1
-
-    # Извлекает срез в обратном порядке.
-    extracted_slice = list1[start_index:end_index + 1][::-1]
-
-    # Удаляет срез, заменяя его пустым.
-    list1[start_index:end_index + 1] = []
-
-    # Добавляет извлечённые элементы к list2.
-    list2.extend(extracted_slice)
-
-    print(list1)
-    print(list2)
-
+    try:
+        list1 = list(map(int, input().split()))
+        list2 = list(map(int, input().split()))
+        
+        start_index = int(input()) - 1
+        end_index = int(input()) - 1
+        
+        # Проверка корректности диапазона.
+        if start_index < 0 or end_index >= len(list1) or start_index > end_index:
+            print("Некорректный диапазон индексов")
+            return
+        
+        # Извлекаем срез в обратном порядке.
+        extracted_slice = list1[start_index:end_index+1][::-1]
+        
+        # Удаляем срез из первого списка.
+        list1[start_index:end_index+1] = []
+        
+        # Добавляем ко второму списку.
+        list2.extend(extracted_slice)
+        
+        print(list1)
+        print(list2)
+        
+    except (ValueError, IndexError):
+        print("Ошибка ввода данных")
 
 if __name__ == "__main__":
     main()
