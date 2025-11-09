518c7ad0c8ba2d52a1dc8b46fa1a0451da01550b
vmolchanova <139213113+mvarukha@users.noreply.github.com>
2025-11-09 15:11:14 +0700

    Create task01.py

---
+++ task01.py
@@ -0,0 +1,11 @@
+def main() -> None:
+    """
+    Считывает 10 чисел, создает новый список
+    из сумм соседних элементов и выводит его.
+    """
+    input_numbers = [int(input()) for _ in range(10)]
+    print([input_numbers[ind] + input_numbers[ind + 1] for ind in range(9)])
+
+
+if __name__ == "__main__":
+    main()
